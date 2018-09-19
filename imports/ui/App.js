06d6e24d3cceeafe9c18eaf4278049d226ddbe6a
Andrew <andrew@mullagaliev.com>
2018-09-19 16:22:50 +0300

    Добавил пару новых действий с задачами

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,103 +1,119 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Meteor } from 'meteor/meteor';
-import { withTracker } from 'meteor/react-meteor-data';
+import {Meteor} from 'meteor/meteor';
+import {withTracker} from 'meteor/react-meteor-data';
 
-import { Tasks } from '../api/tasks.js';
+import {Tasks} from '../api/tasks.js';
 
 import Task from './Task.js';
 import AccountsUIWrapper from './AccountsUIWrapper.js';
 
 // App component - represents the whole app
 class App extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      hideCompleted: false,
-    };
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-
-    // Find the text field via the React ref
-    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
-
-    Meteor.call('tasks.insert', text);
-
-    // Clear form
-    ReactDOM.findDOMNode(this.refs.textInput).value = '';
-  }
-
-  toggleHideCompleted() {
-    this.setState({
-      hideCompleted: !this.state.hideCompleted,
-    });
-  }
-
-  renderTasks() {
-    let filteredTasks = this.props.tasks;
-    if (this.state.hideCompleted) {
-      filteredTasks = filteredTasks.filter(task => !task.checked);
+    componentDidMount() {
+        console.log('mounted');
     }
-    return filteredTasks.map((task) => {
-      const currentUserId = this.props.currentUser && this.props.currentUser._id;
-      const showPrivateButton = task.owner === currentUserId;
 
-      return (
-        <Task
-          key={task._id}
-          task={task}
-          showPrivateButton={showPrivateButton}
-        />
-      );
-    });
-  }
+    constructor(props) {
+        super(props);
 
-  render() {
-    return (
-      <div className="container">
-        <header>
-          <h1>Todo List ({this.props.incompleteCount})</h1>
+        this.state = {
+            hideCompleted: false,
+        };
+        this.removeCompleted = this.removeCompleted.bind(this);
+    }
 
-          <label className="hide-completed">
-            <input
-              type="checkbox"
-              readOnly
-              checked={this.state.hideCompleted}
-              onClick={this.toggleHideCompleted.bind(this)}
-            />
-            Hide Completed Tasks
-          </label>
+    handleSubmit(event) {
+        event.preventDefault();
 
-          <AccountsUIWrapper />
+        // Find the text field via the React ref
+        const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
 
-          { this.props.currentUser ?
-            <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
-              <input
-                type="text"
-                ref="textInput"
-                placeholder="Type to add new tasks"
-              />
-            </form> : ''
-          }
-        </header>
+        Meteor.call('tasks.insert', text);
 
-        <ul>
-          {this.renderTasks()}
-        </ul>
-      </div>
-    );
-  }
+        // Clear form
+        ReactDOM.findDOMNode(this.refs.textInput).value = '';
+    }
+
+    toggleHideCompleted() {
+        this.setState({
+            hideCompleted: !this.state.hideCompleted,
+        });
+    }
+
+    renderTasks() {
+        let filteredTasks = this.props.tasks;
+        if (this.state.hideCompleted) {
+            filteredTasks = filteredTasks.filter(task => !task.checked);
+        }
+        return filteredTasks.map((task) => {
+            const currentUserId = this.props.currentUser && this.props.currentUser._id;
+            const showPrivateButton = task.owner === currentUserId;
+
+            return (
+                <Task
+                    key={task._id}
+                    task={task}
+                    showPrivateButton={showPrivateButton}
+                />
+            );
+        });
+    }
+
+    removeCompleted(event) {
+        event.preventDefault();
+        if (window.confirm('Are you sure you want to delete all completed tasks?')) {
+            Meteor.call('tasks.removeCompleted');
+        }
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <header>
+                    <h1>Todo List ({this.props.completeCount}/{this.props.allCount})</h1>
+
+                    <label className="hide-completed">
+                        <input
+                            type="checkbox"
+                            readOnly
+                            checked={this.state.hideCompleted}
+                            onClick={this.toggleHideCompleted.bind(this)}
+                        />
+                        Hide Completed Tasks
+                    </label>
+                    <button onClick={this.removeCompleted} disabled={!(this.props.completeCount > 0)}>Remove completed tasks
+                    </button>
+
+                    <AccountsUIWrapper/>
+
+                    {this.props.currentUser ?
+                        <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
+                            <input
+                                type="text"
+                                ref="textInput"
+                                placeholder="Type to add new tasks"
+                            />
+                        </form> : ''
+                    }
+                </header>
+
+                <ul>
+                    {this.renderTasks()}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default withTracker(() => {
-  Meteor.subscribe('tasks');
+    Meteor.subscribe('tasks');
 
-  return {
-    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
-    incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
-    currentUser: Meteor.user(),
-  };
+    return {
+        tasks: Tasks.find({}, {sort: {createdAt: -1}}).fetch(),
+        completeCount: Tasks.find({checked: {$eq: true}}).count(),
+        allCount: Tasks.find({}).count(),
+        currentUser: Meteor.user(),
+    };
 })(App);
--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,55 +1,97 @@
-import React, { Component } from 'react';
-import { Meteor } from 'meteor/meteor';
+import React, {Component} from 'react';
+import {Meteor} from 'meteor/meteor';
 import classnames from 'classnames';
 
-import { Tasks } from '../api/tasks.js';
+import {Tasks} from '../api/tasks.js';
+import camelize from '../helpers/camelize';
 
 // Task component - represents a single todo item
 export default class Task extends Component {
-  toggleChecked() {
-    // Set the checked property to the opposite of its current value
-    Meteor.call('tasks.setChecked', this.props.task._id, !this.props.task.checked);
-  }
+    constructor(props) {
+        super(props);
+        this.setCamelize = this.setCamelize.bind(this);
+        this.updateTaskText = this.updateTaskText.bind(this);
+    }
 
-  deleteThisTask() {
-    Meteor.call('tasks.remove', this.props.task._id);
-  }
+    componentDidMount() {
+        console.log('Task mounted');
+    }
 
-  togglePrivate() {
-    Meteor.call('tasks.setPrivate', this.props.task._id, ! this.props.task.private);
-  }
+    componentDidUpdate(prevProps, prevState) {
+        console.log({
+            state: this.state,
+            props: this.props
+        });
+    }
 
-  render() {
-    // Give tasks a different className when they are checked off,
-    // so that we can style them nicely in CSS
-    const taskClassName = classnames({
-      checked: this.props.task.checked,
-      private: this.props.task.private,
-    });
+    toggleChecked() {
+        // Set the checked property to the opposite of its current value
+        Meteor.call('tasks.setChecked', this.props.task._id, !this.props.task.checked);
+    }
 
-    return (
-      <li className={taskClassName}>
-        <button className="delete" onClick={this.deleteThisTask.bind(this)}>
-          &times;
-        </button>
+    deleteThisTask() {
+        Meteor.call('tasks.remove', this.props.task._id);
+    }
 
-        <input
-          type="checkbox"
-          readOnly
-          checked={!!this.props.task.checked}
-          onClick={this.toggleChecked.bind(this)}
-        />
+    togglePrivate() {
+        Meteor.call('tasks.setPrivate', this.props.task._id, !this.props.task.private);
+    }
 
-        { this.props.showPrivateButton ? (
-          <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>
-            { this.props.task.private ? 'Private' : 'Public' }
-          </button>
-        ) : ''}
+    setCamelize(event) {
+        Meteor.call('tasks.nameCamelize', this.props.task._id);
+    }
 
-        <span className="text">
-          <strong>{this.props.task.username}</strong>: {this.props.task.text}
-        </span>
-      </li>
-    );
-  }
+    updateTaskText(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+        const taskId = this.props.task._id;
+
+        if (this.props.task.text !== value) {
+            Meteor.call('tasks.nameChange', this.props.task._id, value);
+        }
+
+    }
+
+    render() {
+        // Give tasks a different className when they are checked off,
+        // so that we can style them nicely in CSS
+        const taskClassName = classnames({
+            checked: this.props.task.checked,
+            private: this.props.task.private,
+        });
+
+        return (
+            <li className={taskClassName}>
+                <button className="delete" onClick={this.deleteThisTask.bind(this)}>
+                    &times;
+                </button>
+
+                <input
+                    type="checkbox"
+                    readOnly
+                    checked={!!this.props.task.checked}
+                    onClick={this.toggleChecked.bind(this)}
+                />
+
+                {this.props.showPrivateButton ? (
+                    <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>
+                        {this.props.task.private ? 'Private' : 'Public'}
+                    </button>
+                ) : ''}
+                <button onClick={this.setCamelize} disabled={this.props.task.text === camelize(this.props.task.text)}>
+                    Camelize name
+                </button>
+                <span className="text">
+                     <strong>{this.props.task.username}</strong>:
+                     <input type="text" value={this.props.task.text}
+                           onChange={this.updateTaskText}/>
+                </span>
+            </li>
+        );
+    }
 }
